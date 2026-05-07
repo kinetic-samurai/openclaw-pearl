d2d902c02a787bacf1227bb0e5351736a06eefc5
Kinetic Samurai <samuraikinetic@gmail.com>
2026-05-07 02:47:08 +0200

    Import Ktor install extension

--- apps/litert-bridge/src/main/kotlin/dev/openclaw/pearl/Main.kt
+++ apps/litert-bridge/src/main/kotlin/dev/openclaw/pearl/Main.kt
@@ -4,6 +4,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
